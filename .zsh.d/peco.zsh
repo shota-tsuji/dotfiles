a738503a44c1714e89cecae506134c5a00cbba48
shota-tsuji <shotatuzi@gmail.com>
2020-09-12 12:50:49 +0900

    feat: add peco-cdr

--- .zsh.d/peco.zsh
+++ .zsh.d/peco.zsh
@@ -41,3 +41,22 @@ function peco-history-selection() {
 zle -N peco-history-selection
 bindkey '^r' peco-history-selection
 
+# cdr
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+	autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+	add-zsh-hook chpwd chpwd_recent_dirs
+	zstyle ':completion:*' recent-dirs-insert both
+	zstyle ':chpwd:*' recent-dirs-default true
+	zstyle ':chpwd:*' recent-dirs-max 1000
+	zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
+fi
+
+function peco-cdr () {
+	local selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
+	if [ -n "$selected_dir" ]; then
+		BUFFER="cd ${selected_dir}"
+		zle accept-line
+	fi
+}
+zle -N peco-cdr
+bindkey '^E' peco-cdr
