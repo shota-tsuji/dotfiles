e6a12c40b4c4ef78a76e2dff4498f44808a8d79c
shota-tsuji <shotatuzi@gmail.com>
2020-09-12 11:42:01 +0900

    feat: add peco and init.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc .vimrc .tmux.conf .gitconfig .gitignore_global .peco)
+DOT_FILES=(.zshrc .vimrc .tmux.conf .gitconfig .gitignore_global)
 for file in ${DOT_FILES[@]}
 do
+	dst_file=$HOME/$file
 	echo $file:
-	ln -s $HOME/dotfiles/$file $HOME/$file
+	#ln -s $HOME/dotfiles/$file $HOME/$file
+	ln -s $(readlink -f $file) $HOME/$file
 done
+
+# init
+ZSH_DIR=$HOME/.zsh.d
+if [ ! -d $ZSH_DIR ]; then
+	mkdir $ZSH_DIR
+fi
 
 DIRS=(.zsh.d)
 for dir in ${DIRS[@]}
 do
-	echo $dir:
-	ln -s $HOME/dotfiles/$dir $HOME/$dir
+	dst_dir=$HOME/$dir
+	if [ -L $dst_dir ]; then
+		echo "skip: " $dst_dir "exists already."
+	else
+		echo "set : " $dst_dir
+		ln -s $(readlink -f $dir) $dst_dir
+	fi
 done
+
+# init
+PECO_HOME=$HOME/.peco
+if [ ! -d $PECO_HOME ]; then
+	mkdir $PECO_HOME
+fi
+
+PECO_CONFIG=.peco/config.json
+echo $HOME/$PECO_CONFIG
+ln -s $(readlink -f $PECO_CONFIG) $HOME/$PECO_CONFIG
