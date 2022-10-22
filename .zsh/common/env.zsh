b1892423e9ee341bf55812786eda1bf0e15173b3
shota-tsuji <shotatuzi@gmail.com>
2022-10-22 14:47:21 +0900

    refactor: Unused settings are commented out.

--- .zsh/common/env.zsh
+++ .zsh/common/env.zsh
@@ -1,19 +1,17 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"	#eval "$(pyenv virtualenv-init -)"
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init -)"	#eval "$(pyenv virtualenv-init -)"
 
-export PATH=$PATH:$(go env GOPATH)/bin
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
+#export PATH=$PATH:$(go env GOPATH)/bin
+#export GOPATH=$HOME/go
+#export PATH=$GOPATH/bin:$PATH
 export EDITOR=vim
 
 bindkey -v
 export PATH="$HOME/bin:$PATH"
 export WORK=$HOME/Work
 
-export LD_LIBRARY_PATH=${HOME}/local/lib/ # boost libraries etc...
-
-export PATH="/home/shota/Work/src/github.com/rbenv/rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+#export PATH="/home/shota/Work/src/github.com/rbenv/rbenv/bin:$PATH"
+#eval "$(rbenv init -)"
 
 eval "$(starship init zsh)"
