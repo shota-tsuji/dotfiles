d698b229452dd7fd63e724f2574837c8233c5f4b
shota-tsuji <shotatuzi@gmail.com>
2022-10-25 08:44:11 +0900

    fix: modify zplug path

--- .zsh/common/plugin.zsh
+++ .zsh/common/plugin.zsh
@@ -1,6 +1,8 @@
 # At first, start "zplug"
 source ~/.zplug/init.zsh
 
-zplug 'wfxr/forgit'
+zplug "wfxr/forgit"
 #zplug "softmoth/zsh-vim-mode", use:zsh-vim-mode.plugin.zsh
 zplug "jeffreytse/zsh-vi-mode"
+
+zplug load
