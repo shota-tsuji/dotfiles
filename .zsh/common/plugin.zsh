0102ba80fba1547e317054c09fabb5e876e78b91
shota-tsuji <shotatuzi@gmail.com>
2022-11-10 23:44:56 +0900

    feat: add pyenv

--- .zsh/common/plugin.zsh
+++ .zsh/common/plugin.zsh
@@ -3,6 +3,7 @@ antigen bundle wfxr/forgit
 
 ZVM_LAZY_KEYBINDINGS=false
 ZVM_INIT_MODE=sourcing
+#ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
 antigen bundle jeffreytse/zsh-vi-mode
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen apply
