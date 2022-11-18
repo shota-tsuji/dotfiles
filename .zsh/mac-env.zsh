9ec0954fc26433fbec5d98d522c09dfcba544d94
shota-tsuji <shotatuzi@gmail.com>
2022-11-18 20:37:52 +0000

    fix: zsh

--- .zsh/mac-env.zsh
+++ .zsh/mac-env.zsh
@@ -1,2 +1,5 @@
 [[ -s "/Users/shota-tsuji/.gvm/scripts/gvm" ]] && source "/Users/shota-tsuji/.gvm/scripts/gvm"
 
+[ -s "/Users/shota-tsuji/.jabba/jabba.sh" ] && source "/Users/shota-tsuji/.jabba/jabba.sh"
+
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
